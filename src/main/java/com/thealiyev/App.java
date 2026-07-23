3f5523e960b61b36ce8a9060c273889d080b9c20
Royal Aliyev <royal.alyv@gmail.com>
2026-07-23 15:33:08 +0400

    23 july 2026, thursday

--- src/main/java/com/thealiyev/App.java
+++ src/main/java/com/thealiyev/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.thealiyev;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/com/thealiyev/AppTest.java
+++ src/test/java/com/thealiyev/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.thealiyev;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
